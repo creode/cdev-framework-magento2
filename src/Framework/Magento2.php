b331205b3f371f9eeb06d9d02641398b025adacf
Gareth Midwood <gareth.midwood@creode.co.uk>
2017-10-22 13:23:33 +0100

    Initial import

---
+++ src/Framework/Magento2.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Cdev\Magento2\Framework;
+
+use Creode\Framework\Framework;
+
+class Magento2 extends Framework
+{
+    const NAME = 'magento2';
+    const LABEL = 'Magento 2';
+
+    const MAGERUN = 'bin/magento';
+
+    /**
+     * Returns commands to clear cache on this framework
+     * @return array
+     */
+    public function clearCache()
+    {
+        return [
+            ['php', self::MAGERUN, 'cache:clean'],
+            ['php', self::MAGERUN, 'cache:flush']
+        ];
+    }
+
+    /**
+     * Returns commands to run updates on this framework
+     * @return array
+     */
+    public function update()
+    {
+        return [
+            ['php', self::MAGERUN, 'setup:upgrade']
+        ];
+    }
+}
